d6c7d479fc85ec8c9854d33f4aff86969c8874a3
Youcef Kharroubi <youcef.kharroubi@soyhuce.fr>
2023-08-08 06:37:06 +0200

    feat: added ECG plot in streamlit UI.

--- ecg_streamlit/ecg_streamlit.py
+++ ecg_streamlit/ecg_streamlit.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
 
 # Title
@@ -12,11 +14,26 @@ st.write(
     to classify whether the ECG is correctly classified or inverted."
 )
 
+
 ecg_file = st.file_uploader(
     label="Choose a CSV file", 
     type="csv",
     accept_multiple_files=False,
 )
+
+colors = {
+    "Correctly realized": "g",
+    "Inverted": "r",
+}
+
+limit = 600
+
+leads_names = ["I", "II", "III", "AVF", "AVL", "AVR", "V1", "V2", "V3", "V4", "V5", "V6"]
+keys = np.arange(12)
+leads_channels = dict(list(zip(keys, leads_names)))
+
+
+
 
 if ecg_file is not None:
 
@@ -32,7 +49,8 @@ if ecg_file is not None:
             index=False,
             header=False,
         )
-        st.write(dataframe)
+        
+        # st.write(dataframe)
 
         files = {'file': open('./tmp/tmp.csv', 'r')}
             
@@ -49,4 +67,28 @@ if ecg_file is not None:
 
         prediction = json_response.get("output")
 
-        st.subheader(f"The prediction is **{json_response}!**")
+        st.subheader(f"The ECG given as input is **{prediction.get('Inversion')}!** \
+                     ({prediction.get('Prediction')})")
+        
+        fig, axs = plt.subplots(12, 1, figsize=(15, 40))
+
+        fig.tight_layout()
+
+        fig.subplots_adjust(hspace=0.5, wspace=0.2)
+
+        for i in range(12):
+            axs[i].plot(
+                dataframe.values[i,:limit], 
+                color=colors[prediction.get('Inversion')]
+            )
+            axs[i].set_title(
+                "{} ECG: {}.".format(
+                    prediction.get('Inversion'), 
+                    leads_channels[i]
+                )
+            )
+
+        for ax in axs.flat:
+            ax.set(xlabel='Time', ylabel='Voltage')
+
+        st.pyplot(fig)
